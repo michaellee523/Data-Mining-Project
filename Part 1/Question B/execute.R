b719a25a9f700479b16c8677d9256abeec5e3315
Jack <jackwongtiongkiat@gmail.com>
2016-12-25 02:21:46 +0800

    Added more plots

--- Part 1/Question B/execute.R
+++ Part 1/Question B/execute.R
@@ -2,27 +2,38 @@ library(ggplot2)
 
 mushroom <- read.csv("mushrooms.csv", header = TRUE, na.strings = "?")
 
+mushroom$class <- factor(mushroom$class, labels = c("Edible", "Poisonous"))
+mushroom$cap.shape <- factor(mushroom$cap.shape, labels = c("Bell", "Conical", "Flat", "Knobbed", "Sunken", "Convex"))
+mushroom$bruises <- factor(mushroom$bruises, labels = c("No bruises", "Bruises"))
+mushroom$cap.color <- factor(mushroom$cap.color, labels = c("Buff", "Cinnamon", "Red", "Gray", "Brown", "Pink", "Green", "Purple", "White", "Yellow"))
 
-capShapePlot <- ggplot(mushroom,aes(x=factor(cap.shape, labels = c("Bell", "Conical", "Flat", "Knobbed", "Sunken", "Convex")),
-fill=factor(class, labels = c("Edible", "Poisonous")))) + geom_bar(position = "dodge", width = 0.5) + 
+capShapePlot <- ggplot(mushroom,aes(x=mushroom),
+fill=class) + geom_bar(position = "dodge", width = 0.5) + 
         xlab("Cap Shape") + ylab("Total Count") + labs(fill = "Mushroom Class") + 
         scale_fill_manual(values=c("green3", "red3"))
 
-bruisesPlot <- ggplot(mushroom, aes(x = factor(bruises, labels = c("No bruises", "Bruises")), fill = 
-                                            factor(class, labels = c("Edible", "Poisonous")))) +
+bruisesPlot <- ggplot(mushroom, aes(x = bruises, fill = class)) +
         geom_bar(position = "dodge", width = 0.5) +
         xlab("Bruises") +
         ylab("Total Count") +
         labs(fill = "Mushroom Class") +
         scale_fill_manual(values=c("green3", "red3"))
 
-capPlot <- ggplot(mushroom, aes(x = factor(cap.color, labels = c("Buff", "Cinnamon", "Red", "Gray", "Brown", "Pink", "Green", "Purple", "White", "Yellow"))
-                                , fill = factor(class, labels = c("Edible", "Poisonous")))) +
+capPlot <- ggplot(mushroom, aes(x = cap.color, fill = class)) +
         geom_bar(position = "dodge", width = 0.5) +
         xlab("Cap Color") +
         ylab("Total Count") +
         labs(fill = "Mushroom Class") +
         scale_fill_manual(values=c("green3", "red3"))
 
+capByBruisePlot <- ggplot(mushroom, aes(x = cap.color, fill = class)) +
+        geom_bar(position = "dodge", width = 0.5) +
+        facet_wrap(~bruises + cap.shape, labeller = label_context) +
+        ggtitle("Bruises, Cap Shape") +
+        xlab("Cap Color") +
+        ylab("Total Count") +
+        labs(fill = "Mushroom Class") +
+        scale_fill_manual(values=c("green3", "red3"))
+
 #plot
-print(capPlot)
+print(capByBruisePlot)
